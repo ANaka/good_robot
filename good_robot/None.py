cac47b7cbf1ff5eebba84b24c7088cee859fce42
anaka <alex.naka@gmail.com>
2020-07-12 09:59:31 -0700

    much new stuff

---
+++ good_robot/None.py
@@ -0,0 +1,369 @@
+
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
+
+# Cell
+#export
+import bezier
+from matplotlib.collections import LineCollection
+from matplotlib import pyplot as plt
+import numpy as np
+from penkit import write, preview
+from pathlib import Path
+import seaborn as sns
+from tqdm import tqdm
+
+from nbdev.showdoc import *
+from .gram import *
+%load_ext autoreload
+%autoreload 2
